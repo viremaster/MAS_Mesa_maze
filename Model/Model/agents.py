5f9eafa6a7ea00138f6685f43466207078c98372
viremaster <rikdesain@gmail.com>
2020-10-12 11:04:01 +0200

    Branch made

--- Model/Model/agents.py
+++ Model/Model/agents.py
@@ -78,7 +78,7 @@ class RedWalker(RandomWalker):
 
 
 class Finish(Agent):
-    # Test
+
     def __init__(self, unique_id, pos, model):
         super().__init__(unique_id, model)
         self.pos = pos
